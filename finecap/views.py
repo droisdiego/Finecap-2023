ac3c7683a17a61f6127f4a41be37531dbf0e15a0
DroisDiego <diegocardosoogeid@gmail.com>
2023-10-05 11:21:48 -0300

    Reservas podem ser visulisadas

--- finecap/views.py
+++ finecap/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from finecap.models import Reserva
@@ -7,5 +8,14 @@ def index(request):
      context = {'reserva':reserva}
      return render (request, 'core/index.html',context)
 
-# def detalhe_reservas(request):
-     
+def detalhe_reserva(request, id):
+     reserva = get_object_or_404(Reserva, id=id)
+     context = {'reserva':reserva}
+     return render(request, 'core/reserva.html', context)
+
+def quitadoresult(request):
+     HttpResponse("oi")
+
+# def cadastrar(request):
+
+
--- main/urls.py
+++ main/urls.py
@@ -14,16 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import statistics
+ 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from finecap.views import index
+from finecap.views import index, detalhe_reserva#, cadastrar
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('', index ,name='index'),
+    path('reserva/<int:id>',detalhe_reserva, name='reserva')
+    # path('cadastrar/', cadastrar,name='cadastrar')
 
-] 
-from django.conf import settings
-from django.conf.urls.static import static
+]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
