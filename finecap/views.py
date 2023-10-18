1978533e3ce238fd45dbd941cf57b64db316a5cd
DroisDiego <diegocardosoogeid@gmail.com>
2023-10-18 16:15:55 -0300

    pequenas modificações no base html

--- finecap/views.py
+++ finecap/views.py
@@ -13,11 +13,6 @@ class Indexview(generic.ListView):
 class DetalheReservaView(generic.DetailView):
     model = Reserva
     template_name = 'core/reserva.html'
-    # context_object_name = 'reserva'
-
-    # def get_object(self, queryset=None):
-    #     id = self.kwargs.get('id')
-    #     return get_object_or_404(Reserva, id=id)
 
 class CadastroView(generic.CreateView):
     model = Reserva
@@ -25,37 +20,14 @@ class CadastroView(generic.CreateView):
     success_url = reverse_lazy('index')
     template_name = 'core/cadastro.html'
     
-    # if request.method == 'POST':
-    #     form = CadastroForms(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         form.save()
-    #         form = CadastroForms()
-    # else:
-    #     form = CadastroForms()
-    # return render(request, 'core/cadastro.html', {'form': form})
-
 class EditarCadastroView(generic.UpdateView):
     model = Reserva
     form_class = CadastroForms
     success_url = reverse_lazy('index')
     template_name = 'core/cadastro.html'
 
-    # reserva = get_object_or_404(Reserva, id=id)
-    # if request.method == 'POST':
-    #     form = CadastroForms(request.POST, instance=reserva)
-    #     if form.is_valid():
-    #         form.save()
-    #         return redirect('index')
-    # else:
-    #     form = CadastroForms(instance=reserva)
-
-    # return render(request, 'core/cadastro.html', {'form': form})
-
-
 class ExcluirCadastroView(generic.DeleteView):
     model = Reserva
     template_name = 'core/reserva_confirm_delete.html'
     success_url = reverse_lazy('index')
-    # reserva = get_object_or_404(Reserva, id=id)
-    # reserva.delete()
-    # return redirect('index')
+    
