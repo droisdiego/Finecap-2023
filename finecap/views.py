fcf360bebc5da90febde63a0e6316992ee1c4405
DroisDiego <diegocardosoogeid@gmail.com>
2023-10-18 20:25:35 -0300

    adição das mensagens

--- finecap/views.py
+++ finecap/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
 from finecap.forms import CadastroForms
 from finecap.models import Reserva
+from django.contrib import messages
 
 class Indexview(generic.ListView):
     model = Reserva
@@ -19,6 +18,10 @@ class CadastroView(generic.CreateView):
     form_class = CadastroForms
     success_url = reverse_lazy('index')
     template_name = 'core/cadastro.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Reserva Criada Com Sucesso!')
+        return super().form_valid(form)
     
 class EditarCadastroView(generic.UpdateView):
     model = Reserva
@@ -26,8 +29,16 @@ class EditarCadastroView(generic.UpdateView):
     success_url = reverse_lazy('index')
     template_name = 'core/cadastro.html'
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Alterações Concluidas!')
+        return super().form_valid(form)
+
 class ExcluirCadastroView(generic.DeleteView):
     model = Reserva
     template_name = 'core/reserva_confirm_delete.html'
     success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'A Reserva Foi Excluída Com Sucesso!')
+        return super().form_valid(form)
     
--- main/settings.py
+++ main/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,6 +70,11 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_TAGS = {
+    messages.SUCCESS: 'success',
+    messages.ERROR: 'danger',
+}
 
 WSGI_APPLICATION = 'main.wsgi.application'
 
